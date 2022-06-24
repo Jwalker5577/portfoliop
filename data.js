a97b266a32bbdc264e7fc55f595d00d255b13904
Jwalker5577 <79887392+Jwalker5577@users.noreply.github.com>
2022-06-23 22:35:21 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Big Lawns",
+    subtitle: "Landscaping Website",
+    description:
+      "This project is a custom website for a landscaping business.  It uses HTML, CSS, and Javascript.  Featuring a responsive navbar that uses Javascript",
+    image: "BL.png",
+    link: "https://walkerjj-biglawns.netlify.app",
+  },
+  {
+    title: "Crypto Tracker App",
+    subtitle: "Cryptocurrency Tracker",
+    description:
+      "This is a crypto tracker built with React.  Uses an API with the use state and use effect hook.  Able to view changes in your cryptocurreny and search for them in the search bar.    ",
+    image: "Cryptoo.png",
+    link: "https://walkerjj-crypto.netlify.app",
+  },
+  {
+    title: "Workout Tracker",
+    subtitle: "Crud Workout Tracker",
+    description:
+      "This project is a CRUD application that logs the name, date, and reps of your workout.  It uses HTML, CSS, and Javascript.",
+    image: "WT.png",
+    link: "https://walkerjj-workouttracker.netlify.app/",
+  },
+  {
+    title: "Quote-Generator App",
+    subtitle: "Inspirational Quotes",
+    description:
+      "A quote generator that uses HTML, CSS, and Javascript.  The app displays inspirational quotes from an API.  You can click a button for a new one and another button to tweet it out.",
+    image: "Quotee.png",
+    link: "https://walkerjj-quotegenerator.netlify.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Bootstrap",
+  "Node",
+  "React",
+  "Wordpress",
+  "MongoDB"
+];
